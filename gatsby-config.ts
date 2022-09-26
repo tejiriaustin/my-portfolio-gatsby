c25889e6d3469452ffbca0adbb4b674736cae036
Tejiri <tejiriaustin123@gmail.com>
2022-09-26 22:42:33 +0100

    add: basic structure for gatsby app

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [`gatsby-plugin-sass`],
 }
 
 export default config

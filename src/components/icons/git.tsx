7e15f6fe369a376d9b422ca482f3c175622c6b0d
hs-tejiri-austin <tejiri.austin@heroshe.com>
2023-02-16 13:20:59 +0100

    add: portfolio details

--- src/components/icons/git.tsx
+++ src/components/icons/git.tsx
@@ -1,14 +1,4 @@
-import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import { Icon } from './types';
 
-export const GitIcon = (): Icon => (
-  <>
-    <StaticImage
-      src="../assets/images/18133.png"
-      alt="A stuff"
-      placeholder="blurred"
-      width={90}
-    />
-  </>
-);
+export const GitIcon = (): Icon => <></>;
